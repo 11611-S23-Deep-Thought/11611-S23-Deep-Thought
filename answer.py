6ed70ac0be2846224bf15f2ada2e1223490f8869
Brandon Wang <brandonwang2000@gmail.com>
2023-03-29 08:39:16 -0400

    1st draft of answer.py pipeline

--- answer.py
+++ answer.py
@@ -1,76 +1,128 @@
 #!/usr/bin/env python3
 
-""" BEGIN: import all necessary NLP modules here! """
+import numpy as np
 import torch
 from transformers import (
     AutoTokenizer,
     AutoModelForQuestionAnswering,
     pipeline
 )
+from sentence_transformers import CrossEncoder
 
-""" END """
-
+from setup import *
+import logging
 import argparse
 import os
 
-# Define all model hyperparameters here (makes it easier to use)
+
+# Define all model configs and hyperparameters
 config = {
-    'model': 'models/roberta-large-synqa-ext', # SynQA EXT
-    "max_length": 400, # max length of input sequence (otherwise, use model's default)
-    "truncation": 'only_second', # never truncate the question, only the context
-    "padding": 'max_length', # how to pad the input sequence
-    "return_overflowing_tokens": True, # return overflowing tokens when after truncation (input sequence > max_length)
-    "return_offsets_mapping": True, # return (char_start, char_end) for each token in the input sequence
-    "stride": 128, # number of tokens b/w truncated and overflowing sequences
-    "n_best_size": 20, # top N answers to pick
-    "max_answer_length": 120, # max length of each answer
-    "batch_size": 128, # batch size
+    'qa_model': SYNQA_LOCAL,
+    'qa_ranker': PASSAGES_LOCAL,
+    'max_length': 400,                      # max length of input sequence
+    'truncation': 'only_second',            # never truncate the question, only the context
+    'padding': 'max_length',                # how to pad the input sequence
+    'return_overflowing_tokens': True,      # return overflowing tokens after truncation (when input sequence > max_length)
+    'return_offsets_mapping': True,         # return (char_start, char_end) for each token in the input sequence
+    'stride': 128,                          # number of tokens b/w truncated and overflowing sequences
+    'top_c': 2,                             # top C contexts/passages to look at
+    'top_a': 3,                             # top K answers to pick
+    'max_answer_length': 120,               # max length of each answer
+    'batch_size': 128,                      # batch size
 }
 
-def main(args):
-
-    article = args.article
-    questions = args.questions
+# Read the text files
+def read_files(article, questions):
+    logging.debug('reading files...')
 
     # Check that text files are valid
     if not os.path.exists(article):
         raise BaseException(f"{article} does not exist")
     if not os.path.exists(questions):
         raise BaseException(f"{questions} does not exist")
-
-    # Read the article
+    
+    # Read the article (and split into useful passages)
+    passages = []
     with open(article, 'r') as f:
-        article = f.read().replace('\n', ' ')
-    print('Article:\n', article[:300])
+        for a in f.read().split('\n'):
+            a = a.strip()
+            if len(a)==0 or a[-1] not in '.!?': # must end in real sentence
+                continue
+            passages.append(a)
 
     # Read the questions
     with open(questions, 'r') as f:
         questions = f.read().splitlines()
-    print('Questions:\n', questions)
+
+    # Debug: print all articles and questions
+    for i, p in enumerate(passages):
+        logging.debug(f'Passage {i}: {p}')
+    for i, q in enumerate(questions):
+        logging.debug(f'Question {i}: {q}')
+
+    return passages, questions
+
+# Find the most relevant passages in the article, then merge into 1 context
+def reduce_article(ranker, q, passages):
+    scores = ranker.predict([(q, passage) for passage in passages])
+    best_idx = np.argsort(scores)[-config['top_c']:]
+    context = ' '.join([passages[i] for i in best_idx])
+    logging.debug(f'Relevant Context: {context}')
+    return context
+
+def main(args):
+
+    article = args.article
+    questions = args.questions
+    passages, questions = read_files(article, questions)
 
     # Use GPU if available (it's fine if not)
     if torch.cuda.is_available():
         DEVICE = torch.device("cuda")
     else:
         DEVICE = torch.device("cpu")
-    print(f'using {DEVICE}')
+    logging.debug(f'using {DEVICE}')
 
     # Build the QA/QG model
-    model = AutoModelForQuestionAnswering.from_pretrained(config['model'])
-    tokenizer = AutoTokenizer.from_pretrained(config['model'])
+    logging.debug('building QA pipeline...')
+    model = AutoModelForQuestionAnswering.from_pretrained(config['qa_model'])
+    tokenizer = AutoTokenizer.from_pretrained(config['qa_model'])
     QA = pipeline('question-answering',
                   model=model,
                   tokenizer=tokenizer,
                   device=DEVICE,
                   framework='pt') # use PyTorch instead of TensorFlow
 
-    # Loop over the questions
+    # Build the passage ranker
+    logging.debug('building ranker...')
+    ranker = CrossEncoder(config['qa_ranker'])
+
+
+    logging.debug('\n\n' + '-'*50 + '\n\n')
+
+
     for q in questions:
-        print('Q:', q)
-        answer = QA(question=q, context=article, top_k=3)
-        print('A:', answer)
+        # Get the list of answers (+confidences) for each question
+        context = reduce_article(ranker, q, passages)
+        answers = QA(question=q, context=context, top_k=config['top_a'])
+        for a in answers:
+            logging.debug('Candidate: %s (%.3f)' % (a['answer'], a['score']))
+        
+        # Print the best answer
+        a = answers[0]['answer']
+        logging.debug(f'Q: {q}')
+        logging.debug(f'A: {a}')
+        logging.debug('- '*10)
+        print(a)
 
 if __name__=='__main__':
+
+    # Define logging level
+    DEBUG = True # print debug messages?
+    level = logging.DEBUG if DEBUG else logging.WARNING
+    logging.basicConfig(level=level)
+
+    # Read the arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('article')   # define first argument
     parser.add_argument('questions') # define second argument
--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from huggingface_hub import snapshot_download
+import os
 
+# Online repo paths
+SYNQA_ONLINE    = 'mbartolo/roberta-large-synqa-ext'
+PASSAGES_ONLINE = 'cross-encoder/ms-marco-TinyBERT-L-2-v2'
+
+# local directory paths
+SYNQA_LOCAL     = 'models/roberta-large-synqa-ext'
+PASSAGES_LOCAL  = 'models/ms-marco-TinyBERT-L-2-v2'
+
+def download_model(repo_id, local_dir, redownload=False):
+    if not redownload and os.path.isdir(local_dir):
+        print(f'{repo_id} is already saved (pass redownload=True to download it again)')
+        return
+
+    # local_dir_use_symlinks=False: no caching or symlinks; put ALL files here!
+    path = snapshot_download(repo_id=repo_id, local_dir=local_dir, local_dir_use_symlinks=False)
+    print(f'Model saved: {repo_id} -> {path}')
 
 if __name__=='__main__':
-    # Download all models/tokenizers from HuggingFace
-    path = snapshot_download(repo_id='mbartolo/roberta-large-synqa-ext', # download all files ...
-                             local_dir='models/roberta-large-synqa-ext', # ... to local system
-                             local_dir_use_symlinks=False) # no caching or symlinks; get ALL files!
-    print(path)
+    # Download all models/tokenizers from HuggingFace to local system (in /models)
+    download_model(SYNQA_ONLINE, SYNQA_LOCAL)
+    download_model(PASSAGES_ONLINE, PASSAGES_LOCAL)
